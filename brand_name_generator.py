0704c599c3eff7fa09a4b9f7d7ce866a339971a6
PEDDI REDDI JAIPAL REDDY <jaipalreddy2002@gmail.com>
2023-08-26 15:09:27 +0530

    My first project

---
+++ brand_name_generator.py
@@ -0,0 +1,5 @@
+#Here is my first project python to create a brandname generator
+print("Welcome to Brand name Generator")
+city_name = input("Which city your are grow up in? \n")
+pet_name = input("What is your pet name? \n")
+print("Your brand name could be : \n"+city_name+" "+pet_name)
